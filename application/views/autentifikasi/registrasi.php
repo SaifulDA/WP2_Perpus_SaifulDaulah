33356b6e192a062e3ca13c806c3fdf0200e05009
SaifulDA <saifuldaulah24@gmail.com>
2021-11-14 06:10:32 +0700

    Web Programming 2

--- application/views/autentifikasi/registrasi.php
+++ application/views/autentifikasi/registrasi.php
@@ -1,61 +1,13 @@
+
 <div class="container">
- <!-- Outer Row -->
- <div class="row justify-content-center">
- <div class="col-lg-7">
- <div class="card o-hidden border-0 shadow-lg my-5">
+ <div class="card o-hidden border-0 bg-warning shadow-lg my-5 col-lg-7 mx-auto">
  <div class="card-body p-0">
  <!-- Nested Row within Card Body -->
  <div class="row">
  <div class="col-lg">
  <div class="p-5">
  <div class="text-center">
- <h1 class="h4 text-grey-900 mb-4">Halaman Masuk</h1>
- </div>
- <?= $this->session->flashdata('pesan'); ?>
- <form class="user" method="post" action="<?= base_url('autentifikasi'); ?>">
- <div class="form-group">
- <input type="text"
-class="form-control form-control-user" value="<?=
-set_value('email'); ?>" id="email" placeholder="isi alamat email" name="email">
- <?= form_error('email',
-'<small class="text-danger pl-3">', '</small>'); ?>
- </div>
- <div class="form-group">
- <input type="password"
-class="form-control form-control-user" id="password"
-placeholder="isi Password" name="password">
- <?= form_error('password',
-'<small class="text-danger pl-3">', '</small>'); ?>
- </div>
- <button type="submit" class="btn 
-btn-success btn-user btn-block">
- Masuk
- </button>
- </form>
-<hr>
- <div class="text-center">
- <a class="small" href="<?= base_url('autentifikasi/lupaPassword'); ?>">Lupa Password ?</a>
- </div>
-<div class="text-center">
- <a class="small" href="<?=base_url('autentifikasi/registrasi'); ?>">Daftar Member !</a>
- </div>
- </div>
- </div>
- </div>
- </div>
- </div>
- </div>
- </div>
-</div>
-<div class="container">
- <div class="card o-hidden border-0 shadow-lg my-5 col-lg-7 mx-auto">
- <div class="card-body p-0">
- <!-- Nested Row within Card Body -->
- <div class="row">
- <div class="col-lg">
- <div class="p-5">
- <div class="text-center">
- <h1 class="h4 text-gray-900 mb-4">Daftar Menjadi Member!</h1>
+ <h1 class="h4 text-white mb-4">Daftar Menjadi Member!</h1>
  </div>
  <form class="user" method="post"
 action="<?= base_url('autentifikasi/registrasi'); ?>">
@@ -87,7 +39,7 @@ name="password2" placeholder="Ulangi Password">
 '<small class="text-danger pl-3">', '</small>'); ?>
  </div>
  </div>
-<button type="submit" class="btn btn-primary btn-user btn-block">
+<button type="submit" class="btn btn-success btn-user btn-block">
  Daftar Menjadi Member
  </button>
  </form>
@@ -95,7 +47,7 @@ name="password2" placeholder="Ulangi Password">
  <div class="text-center">
  <a class="small" href="<?= base_url('autentifikasi/lupaPassword'); ?>">Lupa Password?</a>
  </div>
-<div class="text-center">
+<div class="text-center text-white">
  Sudah Menjadi Member?<a class="small"
 href="<?= base_url('autentifikasi'); ?>"> Login!</a>
  </div>
